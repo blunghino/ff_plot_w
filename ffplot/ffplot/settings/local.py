14f5c72447b63d635cd82a06d71586eb29f51301
blunghino <brentlunghino@gmail.com>
2014-01-26 10:30:14 -0800

    initialized sqlite3 database and admin website

--- ffplot/ffplot/settings/local.py
+++ ffplot/ffplot/settings/local.py
@@ -58,7 +58,7 @@ WSGI_APPLICATION = 'ffplot.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'ffplotdb.sqlite3'),
     }
 }
 
