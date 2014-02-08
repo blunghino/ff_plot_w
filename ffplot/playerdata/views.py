196017fef597661f3d07fd1de1012790543ba5c6
blunghino <brentlunghino@gmail.com>
2014-02-08 15:43:11 -0800

    playerdata_career_data.html page now shows a table of career statistics

--- ffplot/playerdata/views.py
+++ ffplot/playerdata/views.py
@@ -21,7 +21,10 @@ def index(request):
 
 def careerdataview(request, player_name):
 	player = RbPlayer.objects.get(last_name=player_name)
+	field_names = ['career_rushing_yards', 'career_rushing_attempts', 
+					'career_receiving_yards', 'career_receptions']
+	field_data = [getattr(player, f) for f in field_names]
 	template_name = 'playerdata/playerdata_career_data.html'
-	context = {'player': player}
+	context = {'player': player, 'field_names': field_names, 'field_data': field_data}
 	return render(request, template_name, context)
 	
