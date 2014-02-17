baaf69aad6499beab461d12b9e0649510b08b8e8
blunghino <brentlunghino@gmail.com>
2014-02-17 15:40:27 -0800

    finished basic view and template for season data page

--- ffplot/playerdata/urls.py
+++ ffplot/playerdata/urls.py
@@ -4,7 +4,7 @@ from playerdata import views
 
 urlpatterns = patterns('', 
 	url(r'^$', views.PlayersIndexView.as_view(), name='players_index'),
-	url(r'^(?P<urlslug>\w+)/$', views.careerdataview, name='career_data'),
-	url(r'^(?P<urlslug>\w+)/(?P<year>\w+)/$', views.seasondataview, 
+	url(r'^(?P<urlslug>\w+)/$', views.career_data_view, name='career_data'),
+	url(r'^(?P<urlslug>\w+)/(?P<year>\w+)/$', views.season_data_view, 
 		name='season_data'),
 )
--- ffplot/playerdata/views.py
+++ ffplot/playerdata/views.py
@@ -12,13 +12,20 @@ class PlayersIndexView(generic.ListView):
 		return RbPlayer.objects.order_by('last_name')
 	
 
-def careerdataview(request, urlslug):
-	player = RbPlayer.objects.get(urlslug=urlslug)
-	seasons = player.rbseason_set.all()
-	career_field_names = ['career_rushing_attempts', 'career_rushing_yards', 
+def career_data_view(request, urlslug):
+	position = urlslug.split('_')[-1].lower()
+	season_field_names = ['year']
+	if position == 'rb':
+		player = RbPlayer.objects.get(urlslug=urlslug)
+		seasons = player.rbseason_set.all()
+		career_field_names = ['career_rushing_attempts', 'career_rushing_yards', 
 							'career_receptions', 'career_receiving_yards', ]
+		season_field_names += ['season_rushing_attempts', 'season_rushing_yards']
+#	elif position = 'wr':
+#		player = WrPlayer.objects.get(urlslug=urlslug)... etc
+#	else:
+#		return some error or 404
 	career_field_data = [getattr(player, f) for f in career_field_names]
-	season_field_names = ['year', 'season_rushing_attempts', 'season_rushing_yards']
 	season_field_data = [[getattr(s, f) for f in season_field_names] for s in seasons]
 	template_name = 'playerdata/playerdata_career_data.html'
 	context = {
@@ -32,15 +39,32 @@ def careerdataview(request, urlslug):
 	return render(request, template_name, context)
 	
 	
-def seasondataview(request, urlslug, year):
+def season_data_view(request, urlslug, year):
+	position = urlslug.split('_')[-1].lower()
+	season_field_names = ['year']
+	game_field_names = ['game_date', 'opponent', 'home_game']
+	if position == 'rb':
+		player = RbPlayer.objects.get(urlslug=urlslug)
+		season = player.rbseason_set.get(year=year)
+		games = season.rbgame_set.all()
+		season_field_names += ['season_rushing_attempts', 'season_rushing_yards']
+		game_field_names += ['game_rushing_attempts', 'game_rushing_yards']
+	season_field_data = [getattr(season, f) for f in season_field_names]
+	game_field_data = [[getattr(g, f) for f in game_field_names] for g in games]
 	template_name = 'playerdata/playerdata_season_data.html'
-	context = {'year': year}
+	context = {
+				'season': season,
+				'season_field_names': season_field_names,
+				'season_field_data': season_field_data,
+				'game_field_names': game_field_names,
+				'game_field_data': game_field_data,
+		}
 	return render(request, template_name, context)
 	
 	
-# class SeasonDataView(generic.DetailView):
+# class RbSeasonDataView(generic.DetailView):
 # 	template_name = 'playerdata/playerdata_season_data.html'
-# 	player = RbPlayer.objects.get(last_name=player_name)
-# 	season = player.rbseason_set.get(year_string=year_string)
+# 	player = RbPlayer.objects.get(urlslug=urlslug)
+# 	season = player.rbseason_set.get(year=year)
 	
 	
